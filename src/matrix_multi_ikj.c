ae7fc45bcfdc5e2069d36e178183a41741b66a03
SergioCaldas <sergiocaldas.91@gmail.com>
2015-11-26 23:51:03 +0000

    Corrected some bugs

--- src/matrix_multi_ikj.c
+++ src/matrix_multi_ikj.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "papi.h"
-#include <cstdlib>
-#include <iostream>
 #include <sys/time.h>
-#include <string>
-#include <vector>
+#include <string.h>
 #include <omp.h>
 
 #define NUM_EVENTS 2
-#define SIZE 1024
+#define SIZE 1532
 
-void fillMatrices (float **a, float**b, float **c){
-	for (unsigned i=0;i<SIZE;i++){
-		for(unsigned j=0; j<SIZE; j++=){
-			a[i][j] = ((float) rand() / ((float)) RAND_MAX);
+float a[SIZE][SIZE], b[SIZE][SIZE], c[SIZE][SIZE];
+
+void fillMatrices (void){
+	for (unsigned i=0;i<SIZE;++i){
+		for(unsigned j=0; j<SIZE; ++j){
+			a[i][j] = ((float) rand()) / ((float) RAND_MAX);
 			b[i][j] = 1;
 			c[i][j] = 0;
 		}
 	}
 }
 
-void matrix_multiplication(float **a, float **b, float **c, int N){
+void matrix_multiplication(float a[][SIZE], float b[][SIZE], float c[][SIZE], int N){
 	for(unsigned i=0;i<N;i++){
 		for(unsigned k=0;k<N;k++){
 			for(unsigned j=0;j<N;j++){
@@ -33,13 +32,13 @@ void matrix_multiplication(float **a, float **b, float **c, int N){
 
 int main (int argc, char *argv[]){
 
-	float **a,**b,**c;
+	
 
 	int Events[NUM_EVENTS]={PAPI_FP_INS,PAPI_TOT_CYC}; 
 	int EventSet;
 	long long values[NUM_EVENTS];
-
-	fillMatrices(a,b,c);
+	
+	fillMatrices();
 
 	int retval;
 	retval = PAPI_library_init(PAPI_VER_CURRENT);
@@ -48,7 +47,7 @@ int main (int argc, char *argv[]){
 
 	retval = PAPI_start(EventSet);
 	//Matrix Multiplicationi
-	matrix_multiplication(a,b,c);	
+	matrix_multiplication(&a,&b,&c,SIZE);	
 
 	retval = PAPI_stop(EventSet,values);
 
