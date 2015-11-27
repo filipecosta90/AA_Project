d89244a10928966002890b321f8c4f5f127ce5b6
SergioCaldas <sergiocaldas.91@gmail.com>
2015-11-27 23:26:59 +0000

    Organized the validation algorithms

---
+++ src/algorithm_validationBA.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "papi.h"
+#include <sys/time.h>
+#include <string.h>
+#include <omp.h>
+
+#define NUM_EVENTS 2
+#define SIZE 5
+
+void fillMatrices (float **a, float **b, float **c ){
+	for (unsigned i=0;i<SIZE;++i){
+		for(unsigned j=0; j<SIZE; ++j){
+			a[i][j] = ((float) rand()) / ((float) RAND_MAX);
+			b[i][j] = 1;
+			c[i][j] = 0;
+		}
+	}
+}
+
+void matrix_mult_2dot3 (float **a, float **b, float **c, int N){
+	for(unsigned i=0;i<N;i++){
+		for(unsigned k=0;k<N;k++){
+			for(unsigned j=0;j<N;j++){
+				c[i][j] += b[i][k] * a[k][j];
+			}
+		}
+	}
+	FILE *fp;
+	fp = fopen("../Results/BA_multi.csv","w");	
+	for(unsigned i=0;i<N;i++){
+		for(unsigned j=0;j<N;j++){
+			fprintf(fp,"%f,",c[i][j]);	
+		}
+		fprintf(fp,"\n");
+	}
+	fclose(fp);
+}
+
+int main (int argc, char *argv[]){
+	
+	float **mat_a, **mat_b, **mat_c;
+	mat_a = malloc (SIZE*sizeof(float*));
+	mat_b = malloc (SIZE*sizeof(float*));
+	mat_c = malloc (SIZE*sizeof(float*));
+	for(unsigned i=0;i<SIZE;i++){
+		mat_a[i]= malloc ( SIZE * sizeof (float*));
+		mat_b[i]= malloc ( SIZE * sizeof (float*));
+		mat_c[i]= malloc ( SIZE * sizeof (float*));
+	}
+
+	int Events[NUM_EVENTS]={PAPI_FP_INS,PAPI_TOT_CYC}; 
+
+	int EventSet;
+	long long values[NUM_EVENTS];
+
+	//Fill Matrices
+	fillMatrices(mat_a, mat_b, mat_c );
+
+	int retval;
+	retval = PAPI_library_init(PAPI_VER_CURRENT);
+	retval = PAPI_create_eventset(&EventSet);
+	retval = PAPI_add_events(EventSet,Events,NUM_EVENTS);
+
+	retval = PAPI_start(EventSet);
+
+	//Matrix Multiplication
+	matrix_mult_2dot3 ( mat_a, mat_b, mat_c,SIZE);	
+
+	retval = PAPI_stop(EventSet,values);
+
+	for(int i = 0; i<NUM_EVENTS; i++){
+		printf("%lld\n",values[i]);	
+	}
+	return 0;
+}
+
