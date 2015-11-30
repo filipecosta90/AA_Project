f6c9feae4cb8b65ebe11f124d4c5ecd1587c3ecf
filipecosta90 <filipecosta.90@gmail.com>
2015-11-30 00:11:46 +0000

    created 3 tests

---
+++ src/mm_ikj_bytes.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "papi.h"
+
+#define NUM_EVENTS 10
+
+void clearCache(){
+	double clearcache [30000000];
+	for (unsigned i = 0; i < 30000000; ++i)
+		clearcache[i] = i;
+}
+
+void fillMatrices (float **a, float **b, float **c, int N){
+	for (unsigned i=0;i<N;++i){
+		for(unsigned j=0; j<N; ++j){
+			a[i][j] = ((float) rand()) / ((float) RAND_MAX);
+			b[i][j] = ((float) rand()) / ((float) RAND_MAX);
+			c[i][j] = 0.0;
+		}
+	}
+}
+
+void clearMatrix ( float **c, int N){
+	for (unsigned i=0;i<N;++i){
+		for(unsigned j=0; j<N; ++j){
+			c[i][j] = 0.0;
+		}
+	}
+}
+
+void fillMatrices_232 (float **a, float **b, float **c, int N){
+	for (unsigned i=0;i<N;++i){
+		for(unsigned j=0; j<N; ++j){
+			a[i][j] = ((float) rand()) / ((float) RAND_MAX);
+			b[i][j] = 1.0;
+			c[i][j] = 0.0;
+		}
+	}
+}
+
+
+void transpose_matrix (float **b, int N){
+	float aux;
+	for (unsigned i=0;i<N;i++){
+		for(unsigned j=i;j<N;j++){
+			aux = b[i][j];
+			b[i][j]=b[j][i];
+			b[j][i]=aux;
+		}
+	}
+}
+
+
+void fprint_matrix (FILE* fp, float **b, int N){
+	for (unsigned i=0;i<N;i++){
+		for(unsigned j=0;j<N;j++){
+			fprintf(fp,"%f,",b[i][j]);
+		}
+		fprintf(fp,"\n");
+	}
+}
+
+void print_matrix ( float **b, int N){
+	for (unsigned i=0;i<N;i++){
+		for(unsigned j=0;j<N;j++){
+			printf("%f\t",b[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void matrix_mult_ijk (float **a, float **b, float **c, int N){
+	for(unsigned i=0;i<N;i++){
+		for(unsigned j=0;j<N;j++){
+			for(unsigned k=0;k<N;k++){
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+void matrix_mult_ijk_transposed (float **a, float **b, float **c, int N){
+	transpose_matrix(b,N);
+	for(unsigned i=0;i<N;i++){	
+		for(unsigned j=0;j<N;j++){
+			for(unsigned k=0;k<N;k++){
+				c[i][j] += a[i][k] * b[j][k];
+			}
+		}
+	}
+}
+
+void matrix_mult_ikj (float **a, float **b, float **c, int N){
+	for(unsigned i=0;i<N;i++){
+		for(unsigned k=0;k<N;k++){
+			for(unsigned j=0;j<N;j++){
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+int main (int argc, char *argv[]){
+
+	int size = atoi (argv[1]);
+
+	/*PAPI time measurement*/
+	long_long start_us, stop_us;
+
+	/*matrixes*/
+	float **mat_a, **mat_b, **mat_c;
+	mat_a = (float**) malloc (size * sizeof(float*));
+	mat_b = (float**) malloc (size * sizeof(float*));
+	mat_c = (float**) malloc (size * sizeof(float*));
+
+	for (unsigned i = 0; i < size; ++i) {
+		mat_a[i] = (float*) malloc (size * sizeof(float)); 
+		mat_b[i] = (float*) malloc (size * sizeof(float)); 
+		mat_c[i] = (float*) malloc (size * sizeof(float)); 
+	}
+
+	//Fill Matrices
+	fillMatrices(mat_a, mat_b, mat_c, size );
+	clearCache();
+
+	/* PAPI VARIABLES */
+	int events[NUM_EVENTS];
+	long long counts[NUM_EVENTS];
+
+	/* NUMBER OF BYTES TRANSFERED TO/FROM RAM */
+	events[0]=PAPI_L3_TCR;
+	events[1]=PAPI_L3_TCW;
+
+	counts[0] = 0;
+	counts[1] = 0;
+
+	PAPI_library_init(PAPI_VER_CURRENT);
+
+	PAPI_start_counters(events,2);
+	start_us = PAPI_get_real_usec();
+
+
+	matrix_mult_ikj ( mat_a, mat_b, mat_c, size);	
+
+
+	stop_us = PAPI_get_real_usec();
+	PAPI_stop_counters(counts,2);
+
+	long_long duration_us = stop_us - start_us;
+
+	FILE *file;
+	file = fopen(argv[2],"a");
+
+	fprintf(file, "%lld,", duration_us );
+	printf("%lld\t", duration_us );	
+	fprintf(file,"%lld,", counts[0]);	
+	printf("%lld\t", counts[0]);	
+	fprintf(file,"%lld\n", counts[1]);	
+	printf("%lld\n", counts[1]);	
+	fclose(file);
+	return 0;
+}
+
